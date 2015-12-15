08e0f2f0d0980738a1c7df99aaf13cf8e18eaf93
giorgimode <modegiorgi@gmail.com>
2015-12-16 02:09:14 +0400

    added warnings, checks

--- CSVclient/src/main/java/csv/resteasyclient/FileUploadController.java
+++ CSVclient/src/main/java/csv/resteasyclient/FileUploadController.java
@@ -9,7 +9,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
- * Created by giorgi on 16.12.15.
+ * Created by giorgi on 17.12.15.
  */
 public class FileUploadController implements Serializable {
     private static final String parserURL = "http://localhost:8080/csvparser/send/";
@@ -17,22 +17,26 @@ public class FileUploadController implements Serializable {
     public void upload(String filename, String fileContent) {
         try {
 
+            // set connection details here
             URL url = new URL(parserURL+filename);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setDoOutput(true);
             conn.setRequestMethod("POST");
             conn.setRequestProperty("Content-Type", "text/csv");
 
+            // make a POST and free resources
             OutputStream os = conn.getOutputStream();
             os.write(fileContent.getBytes());
             os.flush();
 
+            //check for status
             if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
                 throw new RuntimeException("Failed : HTTP error code : "
                         + conn.getResponseCode());
             }
             else
-            {
+            {   // if successful, notify user
+                //ideally, FacesMessage belongs to View, rather than Controller
                 FacesMessage message = new FacesMessage("Success!", filename + " uploaded successfully");
                 FacesContext.getCurrentInstance().addMessage(null, message);
             }
--- CSVclient/src/main/java/csv/resteasyclient/FileUploadView.java
+++ CSVclient/src/main/java/csv/resteasyclient/FileUploadView.java
@@ -3,13 +3,22 @@ package csv.resteasyclient;
 import org.primefaces.model.UploadedFile;
 
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
 import java.io.Serializable;
 
+/**
+ * Created by giorgi on 17.12.15.
+ */
+
 @Named
 @SessionScoped
-public class FileUploadView implements Serializable{
+public class FileUploadView implements Serializable {
+    //since our class is SessionScoped, it needs to be serializable to be passivated
+
+
     private UploadedFile file;
     private String fileContent;
 
@@ -19,22 +28,33 @@ public class FileUploadView implements Serializable{
     public UploadedFile getFile() {
         return file;
     }
- 
+
+    // when user attaches the file, csv contents are retrieved in string format
     public void setFile(UploadedFile file) {
         setFileContent(new String(file.getContents()));
         this.file = file;
     }
 
     public void upload() {
-        fileUploadController.upload(getFile().getFileName(), getFileContent());
+        // if there is no file or wrong file type attached, user is notified
+        // here we do not cover the case if csv file is corrupt or format is wrong
+      if (!getFile().getContentType().equals("text/csv")) {
+            FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "", "Please Upload a CSV File!");
+            FacesContext.getCurrentInstance().addMessage(null, message);
+        }
+        else {
+            fileUploadController.upload(getFile().getFileName(), getFileContent());
+
+
+        }
     }
 
-    public String getFileContent() {
-
+    // get the content of csv file in string format
+    private String getFileContent() {
         return fileContent;
     }
 
-    public void setFileContent(String fileContent) {
+    private void setFileContent(String fileContent) {
         this.fileContent = fileContent;
     }
 }
